cc614ac5789392f6784870cf80902071df6f6c1e
Sheriff Alhassan <dayo.hassan@gmail.com>
2019-08-09 13:49:37 -0400

    add my notes

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var app = express();
+
 var PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 var htmlroutes = require('./routes/htmlRoutes');
 var apiroutes = require('./routes/apiRoutes');
-app.use(htmlroutes)
+
 app.use(apiroutes)
+app.use(htmlroutes)
+
 app.listen(PORT, function(){
  console.log("App listening on PORT: " + PORT)
 })
